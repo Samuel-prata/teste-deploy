5e264db75b104a380c72d732bbf915fdf16ac9eb
Samuel-prata <samuel.silverio001@hotmail.com>
2024-07-22 19:24:00 -0300

    Able Cors

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 #
 app = Flask(__name__)
+
+#Definindo cors
+CORS(app, origins=['http://localhost:5173/'])
 
 dados = {'nome': 'Samuel', 'idade': 22, 'municipio': 'São José dos Campos'}
 @app.route('/')
