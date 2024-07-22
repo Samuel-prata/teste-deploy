e7d4d38863f321d85c556d61fd3cc500b2c06fbb
Samuel-prata <samuel.silverio001@hotmail.com>
2024-07-22 19:31:06 -0300

    Able all cors

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 #Definindo cors
-CORS(app, origins=['http://localhost:5173/'])
+CORS(app)
 
 dados = {'nome': 'Samuel', 'idade': 22, 'municipio': 'São José dos Campos'}
 @app.route('/')
